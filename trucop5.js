c1e3360c5b2e99a770bdff310c051bb0afee8a3a
Nivaldo Felizola <55410946+Nfelizola@users.noreply.github.com>
2024-03-20 15:26:32 -0300

    truco caotico.. agora falta apenas calcular quem ganha.... cada turno

--- trucop5.js
+++ trucop5.js
@@ -1,13 +1,16 @@
 // VARIAVEIS GLOBAIS A SEREM USADAS NO SISTEMA
 let imgBaralho; // IMAGEM DO BARALHO (COSTA DAS CARTAS)
-let mesaWidth = 1000; // LARGURA DA MESA
-let mesaHeight = 600;// ALTURA DA MESA
+let mesaWidth = 1200; // LARGURA DA MESA
+let mesaHeight = 650;// ALTURA DA MESA
 let jogadorWidth = 200; // LARGURA DA ÁREA DO JOGADOR
 let jogadorHeight = 100; // ALTURA DA ÁREA DO JOGADOR
 let centerAreaWidth = 200; // LARGURA DA AREA CENTRAL 
 let centerAreaHeight = 100;// ALTURA DA AREA CENTRAL
 let inicio=true; // VARIAVEL BOOLENA PARA CONTROLAR SE É A PRIMEIRA CONSTRUÇÃO DE TELA
 let j1,r4;
+let meiomesa=[];
+let turno=[];
+let jacliquei=false;
 const manilhas = [" diamond"," spade"," heart"," clubs"];
 const forcas = ['4', '5', '6', '7', 'Q', 'J', 'K', 'A', '2', '3'];
 let deck = [];
@@ -15,8 +18,8 @@ let maoj1=[];
 let maoj2=[];
 let maoj3=[];
 let maoj4=[];
-let imgc1,imgc2,imgc3,imgvira
-
+let virada;
+let cartaalvo=0;
 // PROTOTIPEI UMA FUNÇÃO SHUFFLE para usar várias vezes no código
 Array.prototype.shuffle = function() {
   for (let i = this.length - 1; i > 0; i--) {
@@ -25,7 +28,6 @@ Array.prototype.shuffle = function() {
   }
   return this;
 };
-
 function preparabaralho(){
 // preparação variaveis de controle do jogo
 for (let naipe of manilhas) {
@@ -35,7 +37,7 @@ for (let naipe of manilhas) {
   }
 }
 deck.shuffle();
-prompt(deck);
+
 maoj1.push(deck.pop());
 maoj1.push(deck.pop());
 maoj1.push(deck.pop());
@@ -48,30 +50,41 @@ maoj3.push(deck.pop());
 maoj4.push(deck.pop());
 maoj4.push(deck.pop());
 maoj4.push(deck.pop());
-let carta1="/image/"+maoj1[0]+".png";
-let carta2= "/image/"+maoj1[1]+".png";
-let carta3= "/image/"+maoj1[2]+".png";
-imgc1=loadImage(carta1);
-imgc2=loadImage(carta2);
-imgc3=loadImage(carta3);
-let vira= deck.pop();
-let cartavira ="/image/"+(vira+".png");
-imgvira=loadImage(cartavira);
-let ref = vira.slice(0,1);
+virada= deck.pop();
+carta1 = new Sprite();
+carta2 = new Sprite();
+carta3 = new Sprite();
+carta1.scale = .42;
+carta1.x=100;
+carta1.y=280;
+carta1.img = "image/"+maoj1[0]+".png";
+carta2.scale = .42;
+carta2.x=150;
+carta2.y=280;
+carta2.img = "image/"+maoj1[1]+".png";
+carta3.scale = .42;
+carta3.x=210;
+carta3.y=280;
+carta3.img = "image/"+maoj1[2]+".png";
+cartavira= new  Sprite();
+cartavira.scale = .40;
+cartavira.x=510;
+cartavira.y=120;
+cartavira.img = "/image/"+virada+".png";
+cartaj2= new  Sprite(-50,0);
+cartaj3= new  Sprite(-100,0);
+cartaj4= new  Sprite(-150,0);
+
+//let ref = vira.slice(0,1);
 //console.log("carta referencia "+ref );
-let refvirada = forcas.lastIndexOf(ref);
-if (refvirada==9){
+//let refvirada = forcas.lastIndexOf(ref);
+//if (refvirada==9){
 //console.log("Todas as "+forcas[0]+" são fortes .. eita manilha veiia");
-}else{
+//}else{
 //console.log("todas as " + forcas[refvirada+1]+" São fortes .. eita manilhada veiaaaaaa uhh hu");
 }
-
-}
-
-
 // a biblioteca P5 PERMITE QUE UTILIZEMOS SKETCHS PADRÕES DE DESENHO E SEMPRE TEREMOS
 // PRELOAD-SETUP - DRAW são funções que trabalham em comjunto.
-
 //---------------------------------------------------------//
 function preload() { // ESTA FUNÇÃO É EXECUTADA ANTES DE SE CARREGAR TODOS OS ITENS DE TELA
    imgBaralho = loadImage('baralho.png'); // Carrega a imagem do baralho (ou use qualquer outra imagem para representar as cartas)
@@ -80,9 +93,13 @@ function preload() { // ESTA FUNÇÃO É EXECUTADA ANTES DE SE CARREGAR TODOS OS
 function setup() {
     // ESTA FUNÇÃO SETUP É PADRÃO DO SISTEMA P5
   createCanvas(mesaWidth, mesaHeight); //CRIAR TELA
-   
 
+     // defino a forma de colisão para carta1
+ 
 }
+function Toaqui(){
+  console.log("sobre a carta1");
+ };
 function draw() {  // função padrão do P5 obrigatoriamente executa ela com frequencia
   drawTable(); // função criada para desenharmos a tabela da mesa 
   // apresenta jogadores se for inicio
@@ -92,13 +109,125 @@ function draw() {  // função padrão do P5 obrigatoriamente executa ela com fr
         r4=prompt("Qual nome do seu Androíde Amigo");
         preparabaralho();
         inicio=false};
-        drawPlayerArea(100, 100,j1); // envia coordenada x e y e playername 
-        drawPlayerArea(mesaWidth - jogadorWidth - 100, 100, "Jogador 2"); // envia coordenada x e y e playername
-        drawPlayerArea(100, mesaHeight - jogadorHeight - 100, "Jogador 3"); // envia coordenada x e y e playername
-        drawPlayerArea(mesaWidth - jogadorWidth - 100, mesaHeight - jogadorHeight - 100, r4); // envia coordenada x e y e playername
-        drawCenterArea();
-
+  drawPlayerArea(100, 100,j1); // envia coordenada x e y e playername 
+  drawPlayerArea(mesaWidth - jogadorWidth - 100, 100, "Jogador 2"); // envia coordenada x e y e playername
+  drawPlayerArea(100, mesaHeight - jogadorHeight - 100, "Jogador 3"); // envia coordenada x e y e playername
+  drawPlayerArea(mesaWidth - jogadorWidth - 100, mesaHeight - jogadorHeight - 100, r4); // envia coordenada x e y e playername
+  drawCenterArea();
+  
 }
+// verificar se o mouse foi pressionado
+function mousePressed() {
+ // o unico que usará o mouse é o Jogador 1 o unico humano, portanto a ação principal se dar[a nesta rotina]
+   // clicado sobre carta1
+  if (mouseX >=(carta1.x)-40 && mouseX <=(carta1.x)+4 && mouseY>=(carta1.y)-40 && mouseY<=(carta1.y)+4){
+  if (confirm(j1+", vai meter este "+maoj1[0]+", mesmo ??") == true){
+    alert("tome esta ladrão...")
+    cartaalvo=1;
+    jacliquei=true;
+    carta1.x=(mesaWidth / 2 - centerAreaWidth / 2)+50;
+    carta1.y=(mesaWidth / 2 - centerAreaWidth / 2)-200;
+      meiomesa.push(maoj1[0]);
+      let escolha2=Math.floor(Math.random()*(maoj2.length-1));
+      let escolha3=Math.floor(Math.random()*(maoj3.length-1));
+      let escolha4=Math.floor(Math.random()*(maoj4.length-1));
+      cartaj2.img = "image/"+maoj2[escolha2]+".png";
+      cartaj2.scale=.42;
+      meiomesa.push(maoj2[escolha2]);
+      cartaj2.x=(mesaWidth / 2 - centerAreaWidth / 2);
+      cartaj2.y=(mesaWidth / 2 - centerAreaWidth / 2)-200;
+      maoj2.splice(escolha2,1);
+      cartaj3.img = "image/"+maoj3[escolha2]+".png";
+      cartaj3.scale=.42;
+      meiomesa.push(maoj3[escolha3]);
+      maoj3.splice(escolha3,1);
+      cartaj3.x=(mesaWidth / 2 - centerAreaWidth / 2);
+      cartaj3.y=(mesaWidth / 2 - centerAreaWidth / 2)-200;
+      cartaj4.img = "image/"+maoj4[escolha4]+".png";
+      cartaj4.scale=.42;
+      maoj4.splice(escolha4,1);
+      meiomesa.push(maoj4[escolha4]);
+      cartaj4.x=(mesaWidth / 2 - centerAreaWidth / 2);
+      cartaj4.y=(mesaWidth / 2 - centerAreaWidth / 2)-200;
+     console.log(meiomesa);
+     alert("agora é calcular qual das cartas é a ganhadora");
+     alert("proxima rodada");
+    }};
+
+ // clicado sobre carta2
+ if (mouseX >=(carta2.x)-40 && mouseX <=(carta2.x)+4 && mouseY>=(carta2.y)-40 && mouseY<=(carta2.y)+4){
+  if (confirm(j1+", vai meter este "+maoj1[1]+", mesmo ??") == true){
+    alert("tome esta ladrão...")
+    cartaalvo=2;
+    jacliquei=true;
+    carta2.x=(mesaWidth / 2 - centerAreaWidth / 2)+50;
+    carta2.y=(mesaWidth / 2 - centerAreaWidth / 2)-100;
+    meiomesa.push(maoj1[1]);
+    let escolha2=Math.floor(Math.random()*(maoj2.length-1));
+    let escolha3=Math.floor(Math.random()*(maoj3.length-1));
+    let escolha4=Math.floor(Math.random()*(maoj4.length-1));
+    cartaj2.img = "image/"+maoj2[escolha2]+".png";
+    cartaj2.scale=.42;
+    meiomesa.push(maoj2[escolha2]);
+    cartaj2.x=(mesaWidth / 2 - centerAreaWidth / 2);
+    cartaj2.y=(mesaWidth / 2 - centerAreaWidth / 2)-200;
+    maoj2.splice(escolha2,1);
+    cartaj3.img = "image/"+maoj3[escolha2]+".png";
+    cartaj3.scale=.42;
+    meiomesa.push(maoj3[escolha3]);
+    maoj3.splice(escolha3,1);
+    cartaj3.x=(mesaWidth / 2 - centerAreaWidth / 2);
+    cartaj3.y=(mesaWidth / 2 - centerAreaWidth / 2)-200;
+    cartaj4.img = "image/"+maoj4[escolha4]+".png";
+    cartaj4.scale=.42;
+    maoj4.splice(escolha4,1);
+    meiomesa.push(maoj4[escolha4]);
+    cartaj4.x=(mesaWidth / 2 - centerAreaWidth / 2);
+    cartaj4.y=(mesaWidth / 2 - centerAreaWidth / 2)-200;
+   console.log(meiomesa);
+   alert("agora é calcular qual das cartas é a ganhadora");
+   alert("proxima rodada");
+  }};
+
+  // clicado sobre carta3
+ if (mouseX >=(carta3.x)-40 && mouseX <=(carta3.x)+4 && mouseY>=(carta3.y)-40 && mouseY<=(carta3.y)+4){
+  if (confirm(j1+", vai meter este "+maoj1[2]+", mesmo ??") == true){
+    alert("tome esta ladrão...")
+    cartaalvo=3;
+    jacliquei=true;
+    carta3.x=(mesaWidth / 2 - centerAreaWidth / 2)+50;
+    carta3.y=(mesaWidth / 2 - centerAreaWidth / 2)-100;
+    meiomesa.push(maoj1[2]);
+    let escolha2=Math.floor(Math.random()*(maoj2.length-1));
+    let escolha3=Math.floor(Math.random()*(maoj3.length-1));
+    let escolha4=Math.floor(Math.random()*(maoj4.length-1));
+    cartaj2.img = "image/"+maoj2[escolha2]+".png";
+    cartaj2.scale=.42;
+    meiomesa.push(maoj2[escolha2]);
+    cartaj2.x=(mesaWidth / 2 - centerAreaWidth / 2);
+    cartaj2.y=(mesaWidth / 2 - centerAreaWidth / 2)-200;
+    maoj2.splice(escolha2,1);
+    cartaj3.img = "image/"+maoj3[escolha2]+".png";
+    cartaj3.scale=.42;
+    meiomesa.push(maoj3[escolha3]);
+    maoj3.splice(escolha3,1);
+    cartaj3.x=(mesaWidth / 2 - centerAreaWidth / 2);
+    cartaj3.y=(mesaWidth / 2 - centerAreaWidth / 2)-200;
+    cartaj4.img = "image/"+maoj4[escolha4]+".png";
+    cartaj4.scale=.42;
+    maoj4.splice(escolha4,1);
+    meiomesa.push(maoj4[escolha4]);
+    cartaj4.x=(mesaWidth / 2 - centerAreaWidth / 2);
+    cartaj4.y=(mesaWidth / 2 - centerAreaWidth / 2)-200;
+   console.log(meiomesa);
+   alert("agora é calcular qual das cartas é a ganhadora");
+   alert("proxima rodada");
+  }};
+}       
+  
+  
+
+  
 //------------------------------------------------------//
 
 // funções que auxiliam o fluxo principal
@@ -126,16 +255,25 @@ function drawCenterArea() {
   // Desenha a área central como um retângulo sem preenchimento
   noFill();
   stroke(0); // Cor da borda preta
-  image(imgvira, centerX +10, centerY + 20, 80,90);
   rect(centerX, centerY, centerAreaWidth, centerAreaHeight+10);
   //desenha a area para as 3 cartas no centro da mesa
-  for (i=0;i<126;i=i+42){
-    fill(34, 139, 187);
-    rect(335+i,210,40,60);
-    }
-    stroke(0); // Cor da borda preta
-    image(imgc1,50,200,70,100);
-    image(imgc2,125,200,70,100);
-    image(imgc3,200,200,70,100);
+  
       }
-
+     
+function limparmeio(){
+ //switch (cartaalvo){
+ // case 1: 
+ // carta1.x=-100;
+ // break;
+ // case 2: 
+ // carta2.x=-100;
+ // break;
+ // case 3: 
+ // carta3.x=-100;
+ // break;
+//};
+ //cartaj2.x=-100;
+ //cartaj3.x=-100;
+ //cartaj4.x=-100;
+  
+}
